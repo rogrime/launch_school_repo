a054ce5d060ed63c0c94bbe10e873324ce9892c8
Ronnie <firedog32@gmail.com>
2019-10-05 05:09:46 -0500

    Added twenty_one game and twenty_one game flow.

---
+++ rb_101/lesson_6/twenty_one.rb
@@ -0,0 +1,163 @@
+CARD_TO_VALUE = { 'J' => 10, 'Q' => 10, 'K' => 10, 'A' => 11 }
+CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+SUITS = ['H', 'S', 'D', 'C']
+WINNING_MSG = { 'user' => 'You Won!',
+                'dealer' => 'You Lose!', 'tie' => 'You Tied!' }
+
+def prompt(msg)
+  puts "==> #{msg}"
+end
+
+def integer?(string)
+  string.to_i.to_s == string || string == '0'
+end
+
+def initialize_deck(suits, cards)
+  suits.product(cards)
+end
+
+def deal_hand!(deck)
+  drawn_cards = deck.sample(2)
+
+  drawn_cards.each { |card| deck.delete(card) }
+  drawn_cards
+end
+
+def hit!(deck, hand)
+  drawn_card = deck.sample
+
+  hand << drawn_card
+  deck.delete(drawn_card)
+end
+
+def total(hand)
+  card_values = hand.map { |cards| cards[1] }
+  card_values.map! { |card| integer?(card) ? card.to_i : CARD_TO_VALUE[card] }
+
+  until card_values.sum < 22 || !card_values.include?(11)
+    ace_index = card_values.index(11)
+    card_values[ace_index] = 1
+  end
+  card_values.sum
+end
+
+def bust?(hand)
+  total(hand) > 21
+end
+
+def tie?(user_sum, dealer_sum)
+  user_sum == dealer_sum
+end
+
+def winner(user_hand, dealer_hand)
+  dealer_sum = total(dealer_hand)
+  user_sum = total(user_hand)
+  user_bust = bust?(user_hand)
+  dealer_bust = bust?(dealer_hand)
+
+  return 'tie' if tie?(user_sum, dealer_sum)
+  return 'user' if dealer_bust || dealer_sum < user_sum && !user_bust
+  'dealer'
+end
+
+def hit_or_stay
+  valid_choices = ['HIT', 'STAY']
+  choice = ''
+
+  loop do
+    prompt('Would you like to \'hit\' or \'stay\' ?:')
+    choice = gets.chomp.upcase
+    break if valid_choices.include?(choice)
+  end
+  choice
+end
+
+def corners_of_cards(hand)
+  top_of_cards = []
+  bottom_of_cards = []
+
+  hand.each do |card|
+    card = card.reverse.join
+    top_of_cards.push("|#{card}" + ' ' * (4 - card.size) + '|')
+    bottom_of_cards.push('|' + ' ' * (4 - card.size) + "#{card}|")
+  end
+  [top_of_cards, bottom_of_cards]
+end
+
+# rubocop:disable Metrics/AbcSize
+def display_cards(user_hand, dealer_hand)
+  corners_of_cards_user = corners_of_cards(user_hand)
+  corners_of_cards_dealer = corners_of_cards(dealer_hand)
+  user_size = user_hand.size
+  dealer_size = dealer_hand.size
+
+  system 'clear'
+  puts <<-CARDS
+
+  (DEALER):
+  #{'+----+ ' * dealer_size}
+  |?   | #{corners_of_cards_dealer[0][1..-1].join(' ')}
+  #{'|    | ' * dealer_size}
+  |   ?| #{corners_of_cards_dealer[1][1..-1].join(' ')}
+  #{'+----+ ' * dealer_size}
+  (YOU):
+  #{'+----+ ' * user_size}
+  #{corners_of_cards_user[0].join(' ')}
+  #{'|    | ' * user_size}
+  #{corners_of_cards_user[1].join(' ')}
+  #{'+----+ ' * user_size}
+  CARDS
+end
+# rubocop:enable Metrics/AbcSize
+
+def display_total(current_total)
+  prompt("Total: #{current_total}")
+end
+
+def display_final_score(user_sum, dealer_sum)
+  prompt("Your Total: #{user_sum} | Dealers Total: #{dealer_sum}")
+end
+
+def display_winning_msg(user_hand, dealer_hand)
+  prompt(WINNING_MSG[winner(user_hand, dealer_hand)])
+end
+
+def play_again?
+  choice = ''
+  choice_to_boolean = { 'Y' => true, 'N' => false }
+  valid_choices = choice_to_boolean.keys
+
+  loop do
+    prompt('Would you like to play again? (Y/N):')
+    choice = gets.chomp.upcase
+    break if valid_choices.include?(choice)
+  end
+  choice_to_boolean[choice]
+end
+
+loop do # Main Loop
+  deck = initialize_deck(SUITS, CARDS)
+  user_hand = deal_hand!(deck)
+  dealer_hand = deal_hand!(deck)
+  current_player_hand = user_hand
+
+  loop do
+    dealer_total = total(dealer_hand)
+    user_total = total(user_hand)
+
+    display_cards(user_hand, dealer_hand)
+    display_total(user_total)
+    break if bust?(current_player_hand)
+
+    user_choice = hit_or_stay unless current_player_hand == dealer_hand
+    current_player_hand = dealer_hand if user_choice == 'STAY'
+
+    break if dealer_total >= 17 && current_player_hand == dealer_hand
+    hit!(deck, current_player_hand)
+  end
+
+  display_cards(user_hand, dealer_hand)
+  display_final_score(total(user_hand), total(dealer_hand))
+  display_winning_msg(user_hand, dealer_hand)
+  break unless play_again?
+end
---
+++ rb_101/lesson_6/twenty_one_flow.rb
@@ -0,0 +1,9 @@
+# 1. Initialize deck
+# 2. Deal cards to user and dealer
+# 3. user turn: hit or stay
+#   - repeat until bust or "stay"
+# 4. If user bust, dealer wins.
+# 5. Dealer turn: hit or stay
+#   - repeat until total >= 17
+# 6. If dealer bust, user wins.
+# 7. Compare cards and declare winner.
